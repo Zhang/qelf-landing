15d942693fd0a934f112a25823b382de53929511
Zhang <Scottzhang235@gmail.com>
2016-01-06 14:04:14 -0800

    update static assets

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
-app.use(express.static('js'));
-app.use(express.static('index.html'));
+app.use(express.static(__dirname + '/js'));
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/fonts'));
+app.use(express.static(__dirname + '/font-awesome'));
+app.use(express.static(__dirname + '/img'));
 
+//app.use(express.static('index.html'));
+app.get('/', function(req, res) {
+  res.send('index.html');
+});
 var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
