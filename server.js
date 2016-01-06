a658f8b958e47ed5773532eb8b6f4e2d7a99b13f
Zhang <Scottzhang235@gmail.com>
2016-01-06 14:10:40 -0800

    works

--- server.js
+++ server.js
@@ -3,20 +3,15 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
+var path = require('path');
 
-app.use(express.static(__dirname + '/js'));
-app.use(express.static(__dirname + '/css'));
-app.use(express.static(__dirname + '/fonts'));
-app.use(express.static(__dirname + '/font-awesome'));
-app.use(express.static(__dirname + '/img'));
+app.use(express.static(__dirname));
 
-//app.use(express.static('index.html'));
-app.get('/', function(req, res) {
-  res.send('index.html');
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
+
 var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
 });
