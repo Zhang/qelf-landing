1cea8fd02069f3c0ffa4d0dadd9871ea984d2a3a
Zhang <Scottzhang235@gmail.com>
2016-01-06 14:13:08 -0800

    works on heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var app = express();
 var port = process.env.PORT || 8080;
 var path = require('path');
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 var server = app.listen(port, function () {
