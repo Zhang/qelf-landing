d6f3c182e502f8954a955d4a1a68adff07f24a25
Zhang <Scottzhang235@gmail.com>
2016-01-06 14:01:33 -0800

    pushing it up

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+'use strict';
+
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
 
 app.use(express.static('js'));
 app.use(express.static('index.html'));
 
-var server = app.listen(3000, function () {
+var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
 
