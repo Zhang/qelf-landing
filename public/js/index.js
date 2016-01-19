3e7fa85e12ec49004759baa6d48f09b3e12f7954
Zhang <Scottzhang235@gmail.com>
2016-01-19 13:18:10 -0800

    alignment

--- public/js/index.js
+++ public/js/index.js
@@ -1,34 +1,34 @@
-$('#signup').click(function() {
-  var emailAddr = $('#email').val();
-
-  function validateEmail(email) {
-    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-    return re.test(email);
-  }
-
-  if (!validateEmail(emailAddr)) {
-    return;
-  }
-
-  var data = {
-    email: emailAddr,
-    referred: $('#referred').val(),
-    phone: $('#phone').val()
-  };
-
-  $('#email').val('');
-  $('#phone').val('');
-  $('#referred').val('');
-
-  $.ajax({
-    type: 'POST',
-    url: 'http://qelf.herokuapp.com',
-    data: data,
-    dataType: 'json'
-  });
-});
-
 $('#signup-btn').click(function() {
   $('#modal').modal();
+  $('#signup').click(function() {
+    var emailAddr = $('#email').val();
+
+    function validateEmail(email) {
+      var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+      return re.test(email);
+    }
+
+    if (!validateEmail(emailAddr)) {
+      return;
+    }
+
+    var data = {
+      email: emailAddr,
+      referred: $('#referred').val(),
+      phone: $('#phone').val()
+    };
+
+    $('#email').val('');
+    $('#phone').val('');
+    $('#referred').val('');
+
+    $.ajax({
+      type: 'POST',
+      url: 'http://qelf.herokuapp.com',
+      data: data,
+      dataType: 'json'
+    });
+    $.modal.close();
+  });
   return false;
 });
